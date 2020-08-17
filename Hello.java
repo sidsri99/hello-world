4ab843bb602957f7a03d8b917571c1b61bba6e03
Siddharth Srivastava <39755416+sidsri99@users.noreply.github.com>
2020-08-17 23:45:35 +0530

    Created Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/**Documentation section
+  Hello world program in Java
+*/
+/*Import Section,
+  optional and
+  if required
+*/
+
+//Class Declaration
+public class Hello{
+  //Main method: Program execution starts from here
+  public static void main(String args[]){
+    //Print Hello World 
+    System.out.println("Hello World");
+  }
+}
